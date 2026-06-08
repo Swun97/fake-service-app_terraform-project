dd0ce5f21f45012a297ff358bd32f82982953a60
Swun97 <55910605+Swun97@users.noreply.github.com>
2026-06-08 22:52:20 +0400

    Add Terraform configuration for AWS provider

---
+++ versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.14"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "swun-devops"
+}
